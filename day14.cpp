f1b373b5700bf9be582c353b5c524638d93e9b77
Eric Bouchard <github@ericbouchard.com>
2020-01-30 01:52:54 -0500

    ajout du jout 14

---
+++ day14.cpp
@@ -0,0 +1,154 @@
+#include "day14.hpp"
+#include "common/string.hpp"
+#include "common/terminal.hpp"
+#include <cassert>
+#include <fmt/format.h>
+#include <map>
+
+struct Elem
+{
+    std::int64_t count = 0;
+    std::string name;
+
+    static Elem Parse(std::string_view data)
+    {
+        Elem el;
+        auto pos = data.find(' ');
+        el.count = std::stoi(std::string{data.substr(0, pos)});
+        el.name = data.substr(pos + 1);
+        trim(el.name);
+        return el;
+    }
+};
+
+struct NanoFactory
+{
+    std::map<std::string, std::pair<Elem, std::vector<Elem>>> reactions;
+    std::map<std::string, std::int64_t> storage;
+
+    static NanoFactory Parse(std::string_view data)
+    {
+        NanoFactory f;
+
+        for (auto const &line : Split(data, '\n'))
+        {
+            auto pos = line.find("=>");
+            auto left = line.substr(0, pos);
+            trim(left);
+
+            std::vector<Elem> composants;
+
+            for (auto &c : Split(left, ','))
+            {
+                trim(c);
+                composants.push_back(Elem::Parse(c));
+            }
+
+            auto right = line.substr(pos + 2);
+            trim(right);
+            auto result = Elem::Parse(right);
+
+            f.reactions[result.name] = std::make_pair(result, std::move(composants));
+        }
+
+        return f;
+    }
+
+    bool Produce(std::string const &name, std::int64_t count = 1)
+    {
+        if (storage[name] >= count)
+            return true;
+
+        if (name == "ORE")
+            return false;
+
+        auto const &[result, composants] = reactions[name];
+        auto n = static_cast<std::int64_t>(
+            ceil((static_cast<double>(count) - storage[name]) / static_cast<double>(result.count)));
+#if 0
+        fmt::print("{} = {} - {} / {}\n", n, count, storage[name], result.count);
+#endif
+
+        bool producing = true;
+
+        for (auto const &c : composants)
+        {
+            producing &= Produce(c.name, n * c.count);
+            storage[c.name] -= n * c.count;
+        }
+
+        if (producing)
+        {
+            storage[name] += n * result.count;
+            return true;
+        }
+
+        return false;
+    }
+
+    std::int64_t ComputeOre()
+    {
+        std::int64_t low = 0;
+        std::int64_t high = std::numeric_limits<std::int64_t>::max();
+
+        while (low < high)
+        {
+            std::int64_t pivot = low + (high - low) / 2;
+            storage.clear();
+            storage["ORE"] = pivot;
+
+            if (Produce("FUEL"))
+                high = pivot;
+            else
+                low = pivot + 1;
+        }
+
+        return low;
+    }
+
+    std::int64_t ComputeFuel(std::int64_t totalOre = 1000000000000)
+    {
+        std::int64_t low = 0;
+        std::int64_t high = totalOre;
+
+        while (low < high - 1)
+        {
+            std::int64_t pivot = low + (high - low) / 2;
+            storage.clear();
+            storage["ORE"] = totalOre;
+
+            if (Produce("FUEL", pivot))
+                low = pivot;
+            else
+                high = pivot - 1;
+        }
+
+        if (Produce("FUEL", high))
+            return high;
+
+        return low;
+    }
+};
+
+int main()
+{
+    fmt::print("Day 14: Space Stoichiometry\n");
+
+    assert(31 == NanoFactory::Parse(example::ex1).ComputeOre());
+    assert(165 == NanoFactory::Parse(example::ex2).ComputeOre());
+    assert(13312 == NanoFactory::Parse(example::ex3).ComputeOre());
+    assert(180697 == NanoFactory::Parse(example::ex4).ComputeOre());
+    assert(2210736 == NanoFactory::Parse(example::ex5).ComputeOre());
+
+    assert(82892753 == NanoFactory::Parse(example::ex3).ComputeFuel());
+    assert(5586022 == NanoFactory::Parse(example::ex4).ComputeFuel());
+    assert(460664 == NanoFactory::Parse(example::ex5).ComputeFuel());
+
+    auto part1 = NanoFactory::Parse(input::data).ComputeOre();
+    fmt::print("  Part1: {}\n", part1);
+    assert(1967319 == part1);
+
+    auto part2 = NanoFactory::Parse(input::data).ComputeFuel();
+    fmt::print("  Part2: {}\n", part2);
+    assert(1122036 == part2);
+}
