bbd564882d6849fd81a1bbd781a5f3b1767e08d7
Eric Bouchard <github@ericbouchard.com>
2020-11-22 01:52:23 -0500

    Ajout du jour 8 de 2015

---
+++ src/2015/day08.cpp
@@ -0,0 +1,9 @@
+#include "day07.hpp"
+#include "../common/assert.hpp"
+#include <fmt/format.h>
+
+int main()
+{
+    // https://adventofcode.com/2015/day/8
+    fmt::print("Day 8: Matchsticks\n");
+}
