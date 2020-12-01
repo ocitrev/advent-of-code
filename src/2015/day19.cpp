cf8de9761483329543a185f89a8d4fe4ed2fb934
Eric Bouchard <github@ericbouchard.com>
2020-11-30 22:05:06 -0500

    Ajout du jour 19

---
+++ src/2015/day19.cpp
@@ -0,0 +1,118 @@
+#include "day19.hpp"
+#include "../common/assert.hpp"
+#include "../common/string.hpp"
+#include <algorithm>
+#include <fmt/format.h>
+#include <functional>
+#include <map>
+#include <random>
+#include <set>
+
+using Grammar = std::multimap<std::string, std::string>;
+
+int CountMolecules(Grammar const &grammar, std::string_view input)
+{
+    std::set<std::string> molecules;
+
+    for (auto const &[from, to] : grammar)
+    {
+        auto pos = input.find(from);
+
+        while (pos != std::string::npos)
+        {
+            std::string inputText{input};
+            inputText.replace(pos, from.size(), to, 0, to.size());
+            molecules.insert(std::move(inputText));
+            pos = input.find(from, pos + from.size());
+        }
+    }
+
+    return static_cast<int>(molecules.size());
+}
+
+Grammar ParseGrammar(std::string_view text)
+{
+    Grammar t;
+    for (auto &&line : Split(text, '\n'))
+    {
+        auto pos = line.find(" => ");
+        t.insert(std::make_pair(line.substr(0, pos), line.substr(pos + 4)));
+    }
+
+    return t;
+}
+
+static auto &GetRNG()
+{
+    static std::mt19937 rng(std::random_device{}());
+    return rng;
+}
+
+int GetNumberOfSteps(Grammar const &grammar, std::string_view input)
+{
+    int count = 0;
+    std::string molecule{input};
+    std::map<std::string, std::string> reverseMap;
+    std::vector<std::string> tos;
+    tos.reserve(grammar.size());
+
+    for (auto const &[from, to] : grammar)
+    {
+        reverseMap.try_emplace(to, from);
+        tos.push_back(to);
+    }
+
+    // du plus grand au plus petit, pour favoriser un algo "greedy"
+    std::sort(begin(tos), end(tos), [](std::string const &a, std::string const &b) {
+        return std::make_pair(a.size(), a) > std::make_pair(b.size(), b);
+    });
+
+    while (molecule != "e")
+    {
+        std::string const old = molecule;
+
+        for (auto const &to : tos)
+        {
+            auto pos = molecule.find(to);
+
+            while (pos != std::string::npos)
+            {
+                auto const &from = reverseMap.find(to)->second;
+                molecule.replace(pos, to.size(), from, 0, from.size());
+                ++count;
+                pos = molecule.find(to, pos + from.size());
+            }
+        }
+
+        if (old == molecule)
+        {
+            count = 0;
+            molecule = input;
+            // si on ne fait pas de progres, randomize l'input
+            // TODO: Implementer l'algo CYK pour eviter de se fier au random
+            // https://en.wikipedia.org/wiki/CYK_algorithm
+            std::shuffle(begin(tos), end(tos), GetRNG());
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    // https://adventofcode.com/2015/day/19
+    fmt::print("Day 19: Medicine for Rudolph\n");
+
+    Assert(4 == CountMolecules(ParseGrammar(example::grammar1), example::molecule1));
+    Assert(7 == CountMolecules(ParseGrammar(example::grammar1), example::molecule2));
+    Assert(3 == GetNumberOfSteps(ParseGrammar(example::grammar2), example::molecule1));
+    Assert(6 == GetNumberOfSteps(ParseGrammar(example::grammar2), example::molecule2));
+
+    auto const part1 = CountMolecules(ParseGrammar(input::grammar), input::molecule);
+    fmt::print("  Part 1: {}\n", part1);
+    Assert(535 == part1);
+
+    auto const part2 = GetNumberOfSteps(ParseGrammar(input::grammar), input::molecule);
+    fmt::print("  Part 2: {}\n", part2);
+    Assert(212 == part2);
+}
---
+++ src/2015/day19.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <string_view>
+
+namespace example
+{
+    constexpr std::string_view grammar1{R"(H => HO
+H => OH
+O => HH)"};
+
+    constexpr std::string_view grammar2{R"(e => H
+e => O
+H => HO
+H => OH
+O => HH)"};
+
+    constexpr std::string_view molecule1{"HOH"};
+    constexpr std::string_view molecule2{"HOHOHO"};
+}
+
+namespace input
+{
+    constexpr std::string_view grammar{R"(Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg)"};
+
+    constexpr std::string_view molecule{
+        "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCa"
+        "SiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSi"
+        "RnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYC"
+        "aSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFA"
+        "rSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"};
+}
