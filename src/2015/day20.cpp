ee9280b80d0a161ab8e924e974c079cb8a7f7c65
Eric Bouchard <github@ericbouchard.com>
2020-12-01 00:27:17 -0500

    Meilleurs noms

--- src/2015/day20.cpp
+++ src/2015/day20.cpp
@@ -7,41 +7,42 @@ int Part1(int limit)
 {
     // la somme des diviseurs est trop lente.
     int const size = limit / 10;
-    std::vector<int> counts(static_cast<std::size_t>(size), 10);
+    std::vector<int> giftsPerHouse(static_cast<std::size_t>(size), 10);
+    
     for (int elve = 2; elve < size; ++elve)
         for (int i = elve; i < size; i += elve)
-            counts[static_cast<std::size_t>(i)] += 10 * elve;
+            giftsPerHouse[static_cast<std::size_t>(i)] += 10 * elve;
 
-    auto iter = std::find_if(begin(counts), end(counts), [limit](int i) {
+    auto iter = std::find_if(begin(giftsPerHouse), end(giftsPerHouse), [limit](int i) {
         return i >= limit;
     });
 
-    return static_cast<int>(std::distance(begin(counts), iter));
+    return static_cast<int>(std::distance(begin(giftsPerHouse), iter));
 }
 
 int Part2(int limit)
 {
     int const size = limit / 10;
-    std::vector<int> counts(static_cast<std::size_t>(size));
+    std::vector<int> giftsPerHouse(static_cast<std::size_t>(size));
 
     for (int elve = 1; elve < size; ++elve)
     {
-        int count = 0;
+        int visited = 0;
 
         for (int i = elve; i < size; i += elve)
         {
-            counts[static_cast<std::size_t>(i)] += 11 * elve;
+            giftsPerHouse[static_cast<std::size_t>(i)] += 11 * elve;
 
-            if (++count == 50)
+            if (++visited == 50)
                 break;
         }
     }
 
-    auto iter = std::find_if(begin(counts), end(counts), [limit](int i) {
+    auto iter = std::find_if(begin(giftsPerHouse), end(giftsPerHouse), [limit](int i) {
         return i >= limit;
     });
 
-    return static_cast<int>(std::distance(begin(counts), iter));
+    return static_cast<int>(std::distance(begin(giftsPerHouse), iter));
 }
 
 int main()
