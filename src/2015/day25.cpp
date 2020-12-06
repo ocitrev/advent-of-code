164d3a67a1a56cf2674319d591e92ce323a7c9af
Eric Bouchard <github@ericbouchard.com>
2020-12-05 23:59:11 -0500

    Ajout du jour 25, 2015

---
+++ src/2015/day25.cpp
@@ -0,0 +1,10 @@
+#include "day25.hpp"
+#include "../common/assert.hpp"
+#include <fmt/format.h>
+
+int main()
+{
+    // https://adventofcode.com/2015/day/25
+    fmt::print("Day 25: Let It Snow\n");
+    fmt::print(input::hint);
+}
---
+++ src/2015/day25.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string_view>
+
+namespace input
+{
+    static constexpr std::string_view hint{
+        "To continue, please consult the code grid in the manual.  Enter the code at row 2981, column 3075."};
+}
