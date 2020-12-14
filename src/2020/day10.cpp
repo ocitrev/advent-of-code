79f4734f44b12080e7e33e175fa77f1d21af390f
Eric Bouchard <github@ericbouchard.com>
2020-12-13 21:42:25 -0500

    Ajout de la partie 2 pour le jour 10, 2020

--- src/2020/day10.cpp
+++ src/2020/day10.cpp
@@ -21,6 +21,55 @@ int Part1(std::vector<int> adapters)
     return diffs[1] * diffs[3];
 }
 
+int Tribbonacci(int n)
+{
+    if (n < 2)
+        return 0;
+
+    int first = 0;
+    int second = 0;
+    int third = 1;
+
+    for (int i = 0; i < n - 2; ++i)
+    {
+        int const next = first + second + third;
+        first = second;
+        second = third;
+        third = next;
+    }
+
+    return third;
+}
+
+int64_t Part2(std::vector<int> adapters)
+{
+    // fortement inspiré de cette solution: https://github.com/colinodell/advent-2020/blob/main/day10/day10.go
+    adapters.insert(begin(adapters), 0);
+    std::sort(begin(adapters), end(adapters));
+    adapters.emplace_back(adapters.back() + 3);
+
+    auto const len = static_cast<int>(adapters.size());
+    int first = 0;
+    int last = 0;
+    int64_t result = 1;
+
+    while (first < len - 1 && last < len - 1)
+    {
+        while (adapters[static_cast<size_t>(last + 1)] - adapters[static_cast<size_t>(last)] == 1)
+            ++last;
+
+        int l = last - first + 1;
+
+        if (l > 2)
+            result *= Tribbonacci(l + 1);
+
+        first = last + 1;
+        last = first;
+    }
+
+    return result;
+}
+
 int main()
 {
     // https://adventofcode.com/2020/day/10
@@ -31,4 +80,8 @@ int main()
     auto const part1 = Part1({begin(input::adapters), end(input::adapters)});
     fmt::print("  Part 1: {}\n", part1);
     Assert(2059 == part1);
+
+    auto const part2 = Part2({begin(input::adapters), end(input::adapters)});
+    fmt::print("  Part 2: {}\n", part2);
+    Assert(86812553324672 == part2);
 }
