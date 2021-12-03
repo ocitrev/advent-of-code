a4a8cf0b6a5c492eb552a071e71582889293805c
Eric Bouchard <github@ericbouchard.com>
2021-12-03 16:15:15 -0500

    Ajout du jour 4, 2021

---
+++ src/2021/day04.cpp
@@ -0,0 +1,27 @@
+#include "../common/assert.hpp"
+#include "day03.hpp"
+#include <fmt/format.h>
+
+static int Part1()
+{
+    return 0;
+}
+
+static int Part2()
+{
+    return 0;
+}
+
+int main()
+{
+    // https://adventofcode.com/2021/day/4
+    fmt::print("Day 4, 2021: \n");
+
+    auto const part1 = Part1();
+    fmt::print("  Part 1: {}\n", part1);
+    // Assert( == part1);
+
+    auto const part2 = Part2();
+    fmt::print("  Part 2: {}\n", part2);
+    // Assert( == part2);
+}
