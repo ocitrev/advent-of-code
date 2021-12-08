90b4df5a8d840d09d0aa816399814cd82968f338
Eric Bouchard <github@ericbouchard.com>
2021-12-07 20:45:17 -0500

    Ajout du jour 8, 2021

---
+++ src/2021/day08.cpp
@@ -0,0 +1,27 @@
+#include "day08.hpp"
+#include "../common/assert.hpp"
+#include <fmt/format.h>
+
+static int Part1()
+{
+    return 0;
+}
+
+static int Part2()
+{
+    return 0;
+}
+
+int main()
+{
+    // https://adventofcode.com/2021/day/8
+    fmt::print("Day 8, 2021: \n");
+
+    auto const part1 = Part1();
+    fmt::print("  Part 1: {}\n", part1);
+    // Assert( == part1);
+
+    auto const part2 = Part2();
+    fmt::print("  Part 2: {}\n", part2);
+    // Assert( == part2);
+}
