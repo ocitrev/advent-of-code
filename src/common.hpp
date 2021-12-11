0c8db94a13d7352660c429ba4cb0b5d737268473
Eric Bouchard <github@ericbouchard.com>
2021-12-11 01:06:20 -0500

    Ajout d'un .h commun pour eviter de tapper

---
+++ src/common.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "common/assert.hpp"
+#include "common/point2d.hpp"
+#include "common/string.hpp"
+#include "common/utils.hpp"
+#include <fmt/format.h>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
