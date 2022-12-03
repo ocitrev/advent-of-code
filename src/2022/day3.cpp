c994a1f448fa1326216030c3eaf4c567e03943df
Eric Bouchard <github@ericbouchard.com>
2022-12-02 22:49:08 -0500

    Ajout du template pour le jour 3

---
+++ src/2022/day3.cpp
@@ -0,0 +1,26 @@
+#include "day3.hpp"
+#include "../common.hpp"
+
+static int Part1()
+{
+    return 0;
+}
+
+static int Part2()
+{
+    return 0;
+}
+
+int main()
+{
+    // https://adventofcode.com/2022/day/3
+    fmt::print("Day 3, 2022: \n");
+
+    auto const part1 = Part1();
+    fmt::print("  Part 1: {}\n", part1);
+    // Assert( == part1);
+
+    auto const part2 = Part2();
+    fmt::print("  Part 2: {}\n", part2);
+    // Assert( == part2);
+}
---
+++ src/2022/day3.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string_view>
+
+namespace example
+{
+  
+}
+
+namespace input
+{
+    
+}
