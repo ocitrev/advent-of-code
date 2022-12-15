935bb3b6e7813b71fdbae362b086fb341d9557f2
Eric Bouchard <github@ericbouchard.com>
2022-12-15 00:06:42 -0500

    Day 15, 2022: Beacon Exclusion Zone

---
+++ src/2022/day15.cpp
@@ -0,0 +1,28 @@
+#include "day15.hpp"
+#include "../common.hpp"
+
+static auto Part1()
+{
+    return 0;
+}
+
+static auto Part2()
+{
+    return 0;
+}
+
+int main()
+{
+    // https://adventofcode.com/2022/day/15
+    fmt::print("Day 15, 2022: Beacon Exclusion Zone\n");
+
+    fmt::print("{}\n", ReadInput());
+
+    auto const part1 = Part1();
+    fmt::print("  Part 1: {}\n", part1);
+    // Assert( == part1);
+
+    auto const part2 = Part2();
+    fmt::print("  Part 2: {}\n", part2);
+    // Assert( == part2);
+}
---
+++ src/2022/day15.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string_view>
+#include <input-bytes.h>
+
+namespace example
+{
+    using namespace std::literals;
+    static constexpr auto text = R"()"sv;
+}
