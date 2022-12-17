a603609e8844dfc789472d4491417a48f499e87b
Eric Bouchard <github@ericbouchard.com>
2022-12-16 21:57:19 -0500

    Day 16, 2022: Proboscidea Volcanium

---
+++ src/2022/day16.cpp
@@ -0,0 +1,26 @@
+#include "day16.hpp"
+#include "../common.hpp"
+
+static auto Part1()
+{
+    return 0;
+}
+
+static auto Part2()
+{
+    return 0;
+}
+
+int main()
+{
+    // https://adventofcode.com/2022/day/16
+    fmt::print("Day 16, 2022: Proboscidea Volcanium\n");
+
+    auto const part1 = Part1();
+    fmt::print("  Part 1: {}\n", part1);
+    // Assert( == part1);
+
+    auto const part2 = Part2();
+    fmt::print("  Part 2: {}\n", part2);
+    // Assert( == part2);
+}
