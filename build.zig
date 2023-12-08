15c7551e6ff66b0b9c075249ccf40dc2551946b9
Eric Bouchard <github@ericbouchard.com>
2023-12-08 02:25:12 -0500

    Day 8, 2023: Haunted Wasteland

--- build.zig
+++ build.zig
@@ -1,33 +1,33 @@
 const std = @import("std");
 
-fn addAoc(b: *std.Build, year: u16, day: u8, target: anytype, optimize: anytype) void {
-    const sourceFile = b.fmt("src/{}/day{}.zig", .{ year, day });
+fn addAoc(b: *std.Build, year: u16, day: u8, target: anytype, optimize: anytype, run_step: anytype, test_step: anytype) void {
+    const source_file = b.fmt("src/{}/day{}.zig", .{ year, day });
     const exe = b.addExecutable(.{
         .name = b.fmt("{}-{}", .{ year, day }),
-        .root_source_file = .{ .path = sourceFile },
+        .root_source_file = .{ .path = source_file },
         .target = target,
         .optimize = optimize,
     });
 
-    const inputFile = b.fmt("inputs/{}/day{}.txt", .{ year, day });
-    exe.addAnonymousModule("input", .{ .source_file = std.build.FileSource.relative(inputFile) });
+    const input_file = b.fmt("inputs/{}/day{}.txt", .{ year, day });
+    exe.addAnonymousModule("input", .{ .source_file = std.build.FileSource.relative(input_file) });
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+    const run_day_step = b.step(b.fmt("run-{}-{}", .{ year, day }), b.fmt("Run day {}", .{day}));
+    run_day_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = sourceFile },
+        .root_source_file = .{ .path = source_file },
         .target = target,
         .optimize = optimize,
     });
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    const runUnitTests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&runUnitTests.step);
 }
 
 // Although this function looks imperative, note that its job is to
@@ -45,5 +45,9 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    addAoc(b, 2023, 1, target, optimize);
+    const run_step = b.step("run", "Run the apps");
+    const test_step = b.step("test", "Run unit tests");
+
+    addAoc(b, 2023, 1, target, optimize, run_step, test_step);
+    addAoc(b, 2023, 8, target, optimize, run_step, test_step);
 }
---
+++ src/2023/day8.zig
@@ -0,0 +1,157 @@
+const std = @import("std");
+
+const Network = struct {
+    left: []const u8,
+    right: []const u8,
+};
+
+const Document = struct {
+    instructions: []const u8,
+    map: std.StringHashMap(Network),
+    allocator: std.mem.Allocator,
+
+    pub fn countSteps(self: *const Document, start: []const u8, comptime predicate: *const fn ([]const u8) bool) u32 {
+        var i: usize = 0;
+        var count: u32 = 0;
+        var next: []const u8 = start;
+
+        while (!predicate(next)) {
+            count += 1;
+            var network = self.map.get(next).?;
+            if (self.instructions[i] == 'L') {
+                next = network.left;
+            } else {
+                next = network.right;
+            }
+            i += 1;
+            i = i % self.instructions.len;
+        }
+
+        return count;
+    }
+
+    fn lcm(a: anytype, b: anytype) @TypeOf(a, b) {
+        return (a * b) / std.math.gcd(a, b);
+    }
+
+    fn endsWithZ(text: []const u8) bool {
+        return std.mem.endsWith(u8, text, "Z");
+    }
+
+    pub fn countGhostSteps(self: *const Document) !u64 {
+        var keyIter = self.map.keyIterator();
+
+        var keys = std.ArrayList([]const u8).init(self.allocator);
+        defer keys.deinit();
+
+        while (keyIter.next()) |k| {
+            if (std.mem.endsWith(u8, k.*, "A")) {
+                try keys.append(k.*);
+            }
+        }
+
+        var total: u64 = 1;
+
+        for (keys.items) |start| {
+            total = lcm(total, self.countSteps(start, &endsWithZ));
+        }
+
+        return total;
+    }
+    pub fn deinit(self: *Document) void {
+        self.map.deinit();
+    }
+};
+
+fn parseDocument(input: []const u8, allocator: std.mem.Allocator) !Document {
+    var it = std.mem.tokenizeAny(u8, input, "\r\n");
+    var result = Document{
+        .instructions = it.next().?,
+        .map = std.StringHashMap(Network).init(allocator),
+        .allocator = allocator,
+    };
+
+    while (it.next()) |line| {
+        var parts = std.mem.splitSequence(u8, line, " = ");
+        const key = parts.first();
+        var leftRight = std.mem.splitAny(u8, std.mem.trim(u8, parts.rest(), "()"), ",");
+        try result.map.put(key, Network{
+            .left = std.mem.trim(u8, leftRight.first(), " "),
+            .right = std.mem.trim(u8, leftRight.rest(), " "),
+        });
+    }
+
+    return result;
+}
+
+fn isZZZ(text: []const u8) bool {
+    return std.mem.eql(u8, text, "ZZZ");
+}
+
+fn part1(input: []const u8) !u32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var doc = try parseDocument(input, allocator);
+    defer doc.deinit();
+
+    return doc.countSteps("AAA", &isZZZ);
+}
+
+test "part 1-a" {
+    const exampleA =
+        \\RL
+        \\
+        \\AAA = (BBB, CCC)
+        \\BBB = (DDD, EEE)
+        \\CCC = (ZZZ, GGG)
+        \\DDD = (DDD, DDD)
+        \\EEE = (EEE, EEE)
+        \\GGG = (GGG, GGG)
+        \\ZZZ = (ZZZ, ZZZ)
+    ;
+    try std.testing.expectEqual(@as(u32, 2), try part1(exampleA));
+    const exampleB =
+        \\LLR
+        \\
+        \\AAA = (BBB, BBB)
+        \\BBB = (AAA, ZZZ)
+        \\ZZZ = (ZZZ, ZZZ)
+    ;
+    try std.testing.expectEqual(@as(u32, 6), try part1(exampleB));
+}
+
+fn part2(input: []const u8) !u64 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var doc = try parseDocument(input, allocator);
+    defer doc.deinit();
+
+    return try doc.countGhostSteps();
+}
+
+test "part 2" {
+    const example =
+        \\LR
+        \\
+        \\11A = (11B, XXX)
+        \\11B = (XXX, 11Z)
+        \\11Z = (11B, XXX)
+        \\22A = (22B, XXX)
+        \\22B = (22C, 22C)
+        \\22C = (22Z, 22Z)
+        \\22Z = (22B, 22B)
+        \\XXX = (XXX, XXX)
+    ;
+    try std.testing.expectEqual(@as(u64, 6), try part2(example));
+}
+
+pub fn main() !void {
+    const input = @embedFile("input");
+    std.debug.print("Day 8, 2023: Haunted Wasteland\n", .{});
+    std.debug.print("  Part 1: {}\n", .{try part1(input)});
+    std.debug.print("  Part 2: {}\n", .{try part2(input)});
+}
