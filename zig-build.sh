a78249109282cb319d89350f40f7dbc0b23a1285
Eric Bouchard <github@ericbouchard.com>
2025-10-28 22:10:34 -0400

    Ajout d'un check pour savoir si zig est intallé

--- zig-build.sh
+++ zig-build.sh
@@ -2,4 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+fatal() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+command -v zig &>/dev/null || fatal Zig is not installed.
+
 zig build --prefix "$SCRIPT_DIR/build/zig" --cache-dir "$SCRIPT_DIR/build/.zig-cache" "$@"
