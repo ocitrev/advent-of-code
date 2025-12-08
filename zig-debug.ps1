cf8dcdfd3b86be0481feb09ce6731e74adce650a
Eric Bouchard <github@ericbouchard.com>
2025-12-07 23:47:53 -0500

    Pour mieux debugger les projets zig

--- zig-debug.ps1
+++ zig-debug.ps1
@@ -1,16 +1,36 @@
 #requires -version 7
 
-[cmdletbinding()]
+[cmdletbinding(defaultparametersetname="exe")]
 param(
-    [parameter(mandatory=$true)]
+    [parameter(mandatory=$true, parametersetname="exe", position=0)]
     [string]$Exe,
 
-    [parameter(mandatory=$true)]
-    [string]$sourceFile
+    [parameter(parametersetname="exe", position=1)]
+    [parameter(mandatory=$true, parametersetname="build", position=0)]
+    [string]$SourceFile,
+
+    [parameter(parametersetname="exe", position=2)]
+    [parameter(parametersetname="build", position=1)]
+    [int]$LineNumber=0,
+
+    [parameter(parametersetname="build")]
+    [switch]$Build
 )
 
 set-strictmode -v latest
 $ErrorActionPreference = 'Stop'
+
+if ($PSCmdLet.ParameterSetName -eq "build")
+{
+    if ($SourceFile -match '\\(\d+)\\day(\d+).zig$') {
+        $year = $matches[1]
+        $day = $matches[2]
+        .\zig-build.cmd "debug-test-$year-$day"
+        return
+    }
+
+    throw "Cannot build source file $SourceFile"
+}
 
 write-verbose 'launching debugger'
 $p = start-process remedybg.exe @($Exe) -passThru
@@ -31,10 +51,10 @@ foreach ($line in (get-content $sourceFile)) {
         )
 
         write-verbose 'open-file'
-        start-process remedybg.exe $args -wait
+        start-process remedybg.exe $args
 
         write-verbose 'add-breakpoint-at-file'
         $args[0] = 'add-breakpoint-at-file'
-        start-process remedybg.exe $args -wait
+        start-process remedybg.exe $args
     }
 }
